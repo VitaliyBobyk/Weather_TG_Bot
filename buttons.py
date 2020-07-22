37fe76458dd93df367b244824234df67475a14ef
VitaliyBobyk <falcone159357bv@gmail.com>
2020-07-22 15:01:30 +0300

    deleted buttons file

--- buttons.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
-
-
-# Main Buttons
-markup_requests = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(
-    KeyboardButton('Отримати погоду по геолокації', request_location=True)).add(
-    KeyboardButton('Отримати погоду по пошуку'))
-
-
